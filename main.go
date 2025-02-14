3228523051f4d360797d23b8bd04b2c3658df68c
Kirill <kirill040605@gmail.com>
2025-02-13 22:26:30 -0800

    updated getCurrentGasInformation func

--- main.go
+++ main.go
@@ -1,42 +1,39 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"example.com/practice/vehicle"
+
 	"example.com/practice/car"
 )
-
-
-
 
 func main() {
 	var mainCar car.Car = car.Car{"Kia", 2024, "Forte GT-line", 400, 305}
 
 	gasType, gasPrice := getCurrentGasInformation() //Menu
 
-
-
-
-
 }
 
-func getCurrentGasInformation() (int, float64){
+func getCurrentGasInformation() (int, float64) {
 	var gasTypes [3]int = [3]int{87, 89, 91}
 	var gasPrices [3]float64 = [3]float64{3.99, 4.19, 4.50}
 
 	fmt.Println("Welcome to the Shell Gas Station:")
 	fmt.Println("What gas type will you use? 87, 89, 91")
-	var userChoice int;
+	var userChoice int
 	for {
 		fmt.Scan(&userChoice)
-		index, err := findGasResult(&gasTypes)
+		index, err := findGasResult(&gasTypes, userChoice)
 
-
+		if err != nil {
+			fmt.Print(err)
+			panic(err)
+		}
 
 		switch userChoice {
 		case 87:
 
-			fmt.Printf("Your gas type is %d, Price: %.2f", gasTypes[])
+			fmt.Printf("Your gas type is %d, Price: %.2f", gasTypes[index])
 		case 89:
 
 		case 91:
@@ -49,17 +46,14 @@ func getCurrentGasInformation() (int, float64){
 
 }
 
-
-func findGasResult(gasTypesArray *[]int, lookingFor int)  (int, error){
-	for i:= 0; i < len(*gasTypesArray); i++ {
+func findGasResult(gasTypesArray *[3]int, lookingFor int) (int, error) {
+	for i := 0; i < len(*gasTypesArray); i++ {
 		currentItem := gasTypesArray[i]
 
-		if (currentItem == lookingFor) {
+		if currentItem == lookingFor {
 			return 0, nil
 		}
 	}
 
-
 	return 0, errors.New("Invalid Index")
 }
-
