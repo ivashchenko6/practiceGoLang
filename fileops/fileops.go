a80fcd79c336f960f87ee3142c34db2b01ed93a9
Kirill <kirill040605@gmail.com>
2025-02-15 09:43:11 -0800

    new file for repetitive functions

---
+++ fileops/fileops.go
@@ -0,0 +1,10 @@
+package fileops
+
+func IncludesIn(elements []int, lookingFor int) bool { // Checks if lookingFor element is in the array/slice. Returning true/false
+
+	for i := 0; i < len(elements); i++ {
+
+	}
+
+	return false
+}
