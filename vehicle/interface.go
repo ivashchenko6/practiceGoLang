6e01a21eac80ee04e7fe66e88db243d05cc410fd
Kirill <kirill040605@gmail.com>
2025-02-17 10:44:07 -0800

    new method Refuel(int). Updated interface

--- vehicle/interface.go
+++ vehicle/interface.go
@@ -1,13 +1,8 @@
 package vehicle
 
 type Vehicle interface {
-	StartEngine()
-	TurnOffEngine()
 	Drive()
-	Brake()
 	CurrentGasoline() int
-}
-
-type Refueler interface {
-	Refuel(float64)
+	MaximumFuelCapacity() int
+	Refuel(int)
 }
