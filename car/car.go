ad1984d616b5128fad5dd82a9f74d3b8cd5e0b30
Kirill <kirill040605@gmail.com>
2025-02-17 10:42:45 -0800

    added 2 new methods to acces fields

--- car/car.go
+++ car/car.go
@@ -1,13 +1,26 @@
 package car
 
+import "fmt"
+
 type Car struct {
-	CarBrand     string
-	CarYear      int
-	CarModel     string
-	FuelCapacity int
-	CurrentFuel  int
+	CarBrand        string
+	CarYear         int
+	CarModel        string
+	MaxFuelCapacity int
+	CurrentFuel     int
 }
 
-func (car *Car) Refuel(moneyAmount float64) {
+func (car Car) Drive() {
+	fmt.Println("Drive")
+}
+func (car Car) CurrentGasoline() int {
+	return car.CurrentFuel
+}
 
+func (car Car) MaximumFuelCapacity() int {
+	return car.MaxFuelCapacity
+}
+
+func (car *Car) Refuel(amount int) {
+	car.CurrentFuel += amount
 }
